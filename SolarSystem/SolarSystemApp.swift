d9028a4f7e342c3d24da50909bc1dd35735510c2
ALL-ALL-ALL <ixartallan5@gmail.com>
2024-03-26 01:21:23 +0100

    Initial Commit

---
+++ SolarSystem/SolarSystemApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolarSystemApp.swift
+//  SolarSystem
+//
+//  Created by  Ixart on 26/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct SolarSystemApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
